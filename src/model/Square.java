8bb3e88cdbe6a6e5f3b7068eb7da9f97826769b6
Daniel Innes <Daniel.w.innes@gmail.com>
2019-10-11 11:44:58 -0400

    Square equals

--- src/model/Square.java
+++ src/model/Square.java
@@ -4,11 +4,27 @@ public class Square {
     private final boolean isTunnel;
     private final boolean isRaised;
     private final Piece piece;
+    private final int hashCode;
 
     Square(boolean isTunnel, boolean isRaised, Piece piece) {
         this.isTunnel = isTunnel;
         this.isRaised = isRaised;
         this.piece = piece;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (isTunnel) {
+            stringBuilder.append(2);
+            stringBuilder.append(2);
+        } else if (isRaised) {
+            stringBuilder.append(1);
+            stringBuilder.append(2);
+        }
+        if (piece == null) {
+            stringBuilder.append(1);
+        } else {
+            stringBuilder.append(2);
+            stringBuilder.append(piece.hashCode());
+        }
+        hashCode = stringBuilder.toString().hashCode();
     }
 
     public boolean hasPiece() {
@@ -25,6 +41,23 @@ public class Square {
 
     boolean isTunnel() {
         return isTunnel;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Square square = (Square) obj;
+        return hasPiece() != square.hasPiece() || isTunnel() != square.isTunnel() || isRaised() != square.isRaised() || !hasPiece() || getPiece().equals(square.getPiece());
+    }
+
+    @Override
+    public int hashCode() {
+        return hashCode;
     }
 
     @Override
