ef5b1f31d0eaaa66113266a5e07b1594e5cfaebe
Daniel Innes <Daniel.w.innes@gmail.com>
2019-10-13 16:20:51 -0400

    Foxes

--- src/controller/Foxes.java
+++ src/controller/Foxes.java
@@ -5,48 +5,48 @@ import model.MoveCommand;
 
 import java.awt.*;
 
-
-class Rabbits {
+class Foxes {
     static boolean checkMove(Board board, MoveCommand moveCommand) {
-        if (board.hasSquare(moveCommand.getTo()) && board.getSquare(moveCommand.getTo()).hasPiece()) {
-            Point point;
-            if (moveCommand.getFrom().x == moveCommand.getTo().x) {
-                if (moveCommand.getFrom().y > moveCommand.getTo().y + 1) {
-                    for (int y = moveCommand.getTo().y + 1; y < moveCommand.getFrom().y - 1; y++) {
+        Point point;
+        switch (board.getSquare(moveCommand.getFrom()).getPiece()) {
+            case FOX_PLUS_Y:
+            case FOX_MINUS_Y:
+                if (moveCommand.getFrom().y > moveCommand.getFrom().y) {
+                    for (int y = moveCommand.getTo().y; y < moveCommand.getFrom().y - 1; y++) {
                         point = new Point(moveCommand.getFrom().x, y);
-                        if (!board.hasSquare(point) || !board.getSquare(point).hasPiece()) {
+                        if (board.hasSquare(point) && board.getSquare(point).hasPiece()) {
                             return false;
                         }
                     }
                     return true;
                 } else if (moveCommand.getFrom().y < moveCommand.getTo().y - 1) {
-                    for (int y = moveCommand.getFrom().y + 1; y < moveCommand.getTo().y - 1; y++) {
+                    for (int y = moveCommand.getFrom().y + 1; y < moveCommand.getTo().y; y++) {
                         point = new Point(moveCommand.getFrom().x, y);
-                        if (!board.hasSquare(point) || !board.getSquare(point).hasPiece()) {
+                        if (board.hasSquare(point) && board.getSquare(point).hasPiece()) {
                             return false;
                         }
                     }
                     return true;
                 }
-            } else if (moveCommand.getFrom().y == moveCommand.getTo().y) {
+            case FOX_PLUS_X:
+            case FOX_MINUS_X:
                 if (moveCommand.getFrom().x > moveCommand.getTo().x + 1) {
-                    for (int x = moveCommand.getTo().x + 1; x < moveCommand.getFrom().x - 1; x++) {
+                    for (int x = moveCommand.getTo().x; x < moveCommand.getFrom().x - 1; x++) {
                         point = new Point(x, moveCommand.getFrom().y);
-                        if (!board.hasSquare(point) || !board.getSquare(point).hasPiece()) {
+                        if (board.hasSquare(point) && board.getSquare(point).hasPiece()) {
                             return false;
                         }
                     }
                     return true;
                 } else if (moveCommand.getFrom().x < moveCommand.getTo().x - 1) {
-                    for (int x = moveCommand.getFrom().x + 1; x < moveCommand.getTo().x - 1; x++) {
+                    for (int x = moveCommand.getFrom().x + 1; x < moveCommand.getTo().x; x++) {
                         point = new Point(x, moveCommand.getFrom().y);
-                        if (!board.hasSquare(point) || !board.getSquare(point).hasPiece()) {
+                        if (board.hasSquare(point) && board.getSquare(point).hasPiece()) {
                             return false;
                         }
                     }
                     return true;
                 }
-            }
         }
         return false;
     }
