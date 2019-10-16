da6cd1d67d1bbf61c09fc58828d5a9036bd2836a
Daniel Innes <Daniel.w.innes@gmail.com>
2019-10-16 13:48:17 -0400

    Piece javaDocs

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,43 +1,57 @@
 package model;
 
 /**
- * A enum Representing the various types of Pieces
- *
- * FOX_PLUS_X: A part of the fox facing the positive x direction
- * FOX_MINUS_X: A part of the fox facing the negative x direction
- * etc.
+ * A enum Representing the various types of Pieces.
  */
 public enum Piece {
+    /**
+     * The mushroom game piece.
+     */
     MUSHROOM {
         @Override
         public String toString() {
             return "Mushroom";
         }
     },
+    /**
+     * The rabbit game piece.
+     */
     RABBIT {
         @Override
         public String toString() {
             return "Rabbit";
         }
     },
+    /**
+     * A part of the fox facing the plus x direction
+     */
     FOX_PLUS_X {
         @Override
         public String toString() {
             return "Fox";
         }
     },
+    /**
+     * A part of the fox facing the minus x direction
+     */
     FOX_MINUS_X {
         @Override
         public String toString() {
             return "Fox";
         }
     },
+    /**
+     * A part of the fox facing the plus y direction
+     */
     FOX_PLUS_Y {
         @Override
         public String toString() {
             return "Fox";
         }
     },
+    /**
+     * A part of the fox facing the minus y direction
+     */
     FOX_MINUS_Y {
         @Override
         public String toString() {
