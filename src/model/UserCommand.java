c036c5660290acc5beeefa4dede677f30fd81791
Daniel Innes <Daniel.w.innes@gmail.com>
2019-10-16 12:50:32 -0400

    UserCommand javaDocs

--- src/model/UserCommand.java
+++ src/model/UserCommand.java
@@ -5,8 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for method in game that can be run from the view.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface UserCommand {
+    /**
+     * Get the description of the method. This description is meant for user to read from a help call in the view.
+     *
+     * @return The method description
+     */
     String description();
 }
