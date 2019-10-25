0e504dc4dbca1447f7771ddfca63a3e4a62253fd
Daniel Innes <Daniel.w.innes@gmail.com>
2019-10-25 09:08:27 -0400

    getMoves offset rabbits

--- src/helpers/Rabbit.java
+++ src/helpers/Rabbit.java
@@ -8,50 +8,30 @@ import java.util.List;
 import java.util.Map;
 
 public class Rabbit implements Piece {
-    @Override
-    public Map<Move, List<Move>> getMoves(Board board, Point start) {
+
+    private Map<Move, List<Move>> getMoves(Board board, Point start, Point offset) {
         boolean c = true;
         Map<Move, List<Move>> moves = new HashMap<>();
         Move move;
         Point point = new Point(start);
         while (c) {
-            point = new Point(point.x, point.y + 1);
+            point = new Point(point.x + offset.x, point.y + offset.y);
             c = board.hasPiece(point);
         }
-        if (point.y <= board.getMax().y && !start.equals(new Point(point.x, point.y - 1))) {
+        if (!start.equals(new Point(point.x - offset.x, point.y - offset.y)) && point.y <= board.getMax().y && point.x <= board.getMax().x && point.y >= 0 && point.x >= 0) {
             move = new Move(start, point);
             moves.put(move, List.of(move));
         }
-        point = new Point(start);
-        c = true;
-        while (c) {
-            point = new Point(point.x, point.y - 1);
-            c = board.hasPiece(point);
-        }
-        if (point.y >= 0 && !start.equals(new Point(point.x, point.y + 1))) {
-            move = new Move(start, point);
-            moves.put(move, List.of(move));
-        }
-        point = new Point(start);
-        c = true;
-        while (c) {
-            point = new Point(point.x + 1, point.y);
-            c = board.hasPiece(point);
-        }
-        if (point.x <= board.getMax().x && !start.equals(new Point(point.x - 1, point.y))) {
-            move = new Move(start, point);
-            moves.put(move, List.of(move));
-        }
-        point = new Point(start);
-        c = true;
-        while (c) {
-            point = new Point(point.x - 1, point.y);
-            c = board.hasPiece(point);
-        }
-        if (point.x >= 0 && !start.equals(new Point(point.x + 1, point.y))) {
-            move = new Move(start, point);
-            moves.put(move, List.of(move));
-        }
+        return moves;
+    }
+
+    @Override
+    public Map<Move, List<Move>> getMoves(Board board, Point start) {
+        Map<Move, List<Move>> moves = new HashMap<>();
+        moves.putAll(getMoves(board, start, new Point(0, 1)));
+        moves.putAll(getMoves(board, start, new Point(0, -1)));
+        moves.putAll(getMoves(board, start, new Point(1, 0)));
+        moves.putAll(getMoves(board, start, new Point(-1, 0)));
         return moves;
     }
 
