8229677de974e1359f74fe1a682eab73278fd836
Binyamin-Brion <36389791+Binyamin-Brion@users.noreply.github.com>
2019-11-04 22:39:39 -0500

    Update View.java
    - Added java doc.

--- src/view/View.java
+++ src/view/View.java
@@ -5,6 +5,10 @@ import model.Board;
 
 import java.awt.*;
 
+/**
+ *  The interface that a view must implement.
+ */
+
 public interface View {
     void addPiece(Point point, Piece piece);
     void removePiece(Point point);
