75fdeb40717face600fac3efc44aca10d4bf572f
Daniel Innes <Daniel.w.innes@gmail.com>
2019-11-15 16:30:56 -0500

    Some comments

--- src/main/java/backend/helpers/Fox.java
+++ src/main/java/backend/helpers/Fox.java
@@ -20,16 +20,17 @@ public final class Fox extends Piece {
                 .hash();
     }
 
+    //This function determines the possible for the fox in one direction, given one orientation
     private Set<Move> getMoves(Board board, Point start, Point offset, boolean isMovingBackwards) {
         Set<Move> moves = new HashSet<>();
-        Point point = new Point(start);
-        while (true) {
-            point = new Point(point.x + offset.x, point.y + offset.y);
-            if (!board.hasPiece(point) && point.y <= board.getMax().y && point.x <= board.getMax().x && point.y >= 0 && point.x >= 0) {
-                if ((!isMovingBackwards && start.equals(head)) || isMovingBackwards && start.equals(tail)) {
-                    moves.add(new Move(this, new Fox(new Point(point.x - offset.x, point.y - offset.y), point)));
+        Point point = new Point(start);//The main loop operator / pointer
+        while (true) { //Loop to look along a line in the offset direction
+            point = new Point(point.x + offset.x, point.y + offset.y); //Move the point by one offset
+            if (!board.hasPiece(point) && point.y <= board.getMax().y && point.x <= board.getMax().x && point.y >= 0 && point.x >= 0) { //Check if the if there is no piece, and if the piece is on the board.
+                if ((!isMovingBackwards && start.equals(head)) || isMovingBackwards && start.equals(tail)) { //This determines which case (line 46, 47, 50, 51) the function was called from and therefore if the head is on the trailing side or the leading side of the loop.
+                    moves.add(new Move(this, new Fox(new Point(point.x - offset.x, point.y - offset.y), point))); // If the head is on the trailing side of the loop it is initiated to the loop pointer minus the offset.
                 } else {
-                    moves.add(new Move(this, new Fox(point, new Point(point.x - offset.x, point.y - offset.y))));
+                    moves.add(new Move(this, new Fox(point, new Point(point.x - offset.x, point.y - offset.y)))); // If the head is not on the trailing side of the loop it is initiated to the loop pointer minus the offset.
                 }
             } else {
                 break;
@@ -38,14 +39,20 @@ public final class Fox extends Piece {
         return moves;
     }
 
+    // Note:
+    // There are two likely possibilities for this error.
+    //  1.  The following cases are not inclusive and I have missed a case.
+    //  2.  The if statement on line 29 is not properly determining which case it is currently processing.
 
+    //This is the function that is initially called by the game in order to determine possible moves from a given end of the fox.
     @Override
     public Set<Move> getMoves(Board board, Point point) {
         Set<Move> moves = new HashSet<>();
-        if (head.x == tail.x) {
+        if (head.x == tail.x) { //Check if the fox is aligned on the x plane, by checking that the front and back end both have the same x value
             moves.addAll(getMoves(board, point, new Point(0, 1), false));
             moves.addAll(getMoves(board, new Point(point.x, point.y - 1), new Point(0, -1), true));
-        } else {
+        } else {//Check if the fox is aligned on the x plane, by checking that the front and back end both have the same y value
+            // All the logic for the y plane is the same as the x plane.
             moves.addAll(getMoves(board, point, new Point(1, 0), false));
             moves.addAll(getMoves(board, new Point(point.x - 1, point.y), new Point(-1, 0), true));
         }
